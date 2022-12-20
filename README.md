01e31d8d1366621d0bda3c79eceba5c670f977a1
Ben Hirsch <benhirsch42@gmail.com>
2022-12-19 23:39:37 -0500

    Start reimplementing without sqlite

--- README.md
+++
@@ -1,7 +0,0 @@
-# bevy_economy
-
-Status: Code salad that compiles sometimes.
-
-A plugin for the Rust game engine [Bevy](https://github.com/bevyengine/bevy) that adds a component to let entities participate in a simulated market economy.
-
-Based on the paper [Emergent Economies for Role Playing Games](https://ianparberry.com/pubs/econ.pdf).
--- examples/small_market.rs
+++ examples/small_market.rs
@@ -2,11 +2,21 @@ extern crate bevy_economy;
 
 use bevy::{prelude::*, window::close_on_esc};
 use bevy_economy::EconomyPlugin;
+use fake::Fake;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(EconomyPlugin)
+        .add_startup_system(seed_world)
         .add_system(close_on_esc)
         .run();
+}
+
+fn seed_world() -> Result<()> {
+    use fake::faker::name::raw::*;
+    use fake::locales::*;
+
+    let agents = (0..10).into_iter().map(|_| NewAgent::new(Name(EN).fake()));
+    Ok(())
 }
